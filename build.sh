9002902135e38ac8381e8b25c1d0801a1fbe9bc4
Wojciech Piętka <wojciech.pietka13@gmail.com>
2023-10-12 14:14:22 +0200

    Don't save vertical space on refrains

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+upload() {
+	aws s3 cp songbook.pdf s3://piewoj/songbook.pdf
+	aws s3api put-object-tagging --bucket piewoj --key songbook.pdf --tagging 'TagSet={Key=public,Value=yes}'
+}
+
 python3 ./python/convert.py
 pdflatex -halt-on-error ./songbook.tex > /dev/null
 pdflatex -halt-on-error ./songbook.tex > /dev/null
@@ -8,3 +13,5 @@ rm ./songbook.aux
 rm ./songbook.log
 rm ./songbook.out
 rm ./songbook.toc
+
+if [ "$1" = "upload" ]; then upload; fi
